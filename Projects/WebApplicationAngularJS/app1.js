a7087ba1f7e0e8800cb0ede6bcd6350e800a6ad6
Victor Jordão <vic2198@hotmail.com>
2017-08-08 00:33:08 -0300

    Dados gridOptions por get arquivo json

--- Projects/WebApplicationAngularJS/app1.js
+++ Projects/WebApplicationAngularJS/app1.js
@@ -19,24 +19,20 @@
     var app = angular.module("app1", ['ngGrid']);
 
     app.controller('HomeController', function ($scope, $http) {
-        $scope.Data = [];       
-        $http.get('/Home/GetPessoas').then(function (data, status, headers, config) {
-            $scope.Data = [{ name: "Moroni", age: 50 },
-        { name: "Tiancum", age: 43 },
-        { name: "Jacob", age: 27 },
-        { name: "Nephi", age: 29 },
-        { name: "Enos", age: 34 }];
+        $scope.gridOptions = {
+            data: 'data',
+            columnDefs: [
+                { field: 'Name', displayName: 'Nome' },
+                { field: 'Idade', displayName: 'Idade' }
+            ]
+        };
+       $http.get('/Home/GetPessoas').then(function (response) {
+           $scope.data = response.data
 
         }, function (error) {
             console.log(data);
         });
-        $scope.gridOptions = {
-            data: 'Data',
-            columnDefs: [
-                { field: 'Nome', displayName: 'Nome' },
-                { field: 'Idade', displayName: 'Idade' }
-            ]
-        };
+        
     });
 
 })();
